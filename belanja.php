c5bda74b7734ff8e5583af43a0779b337cef62e1
RadjaShiqnals <radjagenta@gmail.com>
2026-07-16 12:36:28 +0700

    style: improve shopping page card alignment and styling

--- belanja.php
+++ belanja.php
@@ -14,31 +14,39 @@ include "header.php";
 
     ?>
 
-        <div class="col-md-3">
+        <div class="col-md-3 mb-4 d-flex align-items-stretch">
 
-            <div class="card border border-primary shadow-0 text-white mb-3" style="margin:10px;background-color:#0036b3;">
+            <div class="card border border-primary shadow-0 text-white w-100" style="background-color:#0036b3; display: flex; flex-direction: column;">
 
                 <img src="assets/img/<?php echo $data_barang['foto']; ?>" class="card-img-top" style="height: 300px;object-fit: contain;margin-top: 5px;">
 
-                <div class="card-body">
+                <div class="card-body d-flex flex-column">
 
-                    <h5 class="card-title"><?php echo $data_barang['nama_barang']; ?></h5>
+                    <h5 class="card-title"><?php echo htmlspecialchars($data_barang['nama_barang']); ?></h5>
 
-                    <p class="card-text">
-                        Pemilik Asli : <?php echo $data_barang['pengarang']; ?>
+                    <p class="card-text mb-1">
+                        Pemilik Asli : <?php echo htmlspecialchars($data_barang['pengarang']); ?>
                     </p>
 
-                    <p class="card-text">
-                        Harga : <?php echo $hargabarang; ?>
+                    <p class="card-text mb-3">
+                        Harga : <?php echo htmlspecialchars($hargabarang); ?>
                     </p>
 
-                    <p class="card-text">
-                        <?php echo substr($data_barang['deskripsi'], 0, 255); ?>
+                    <?php 
+                    $deskripsi = $data_barang['deskripsi'];
+                    if (strlen($deskripsi) > 100) {
+                        $deskripsi = substr($deskripsi, 0, 97) . '...';
+                    }
+                    ?>
+                    <p class="card-text flex-grow-1">
+                        <?php echo htmlspecialchars($deskripsi); ?>
                     </p>
 
-                    <a href="beli_barang.php?id_barang=<?php echo $data_barang['id_barang']; ?>" class="btn btn-light btn-lg px-5">
-                        Beli
-                    </a>
+                    <div class="mt-auto">
+                        <a href="beli_barang.php?id_barang=<?php echo $data_barang['id_barang']; ?>" class="btn btn-light btn-lg w-100 text-center">
+                            Beli
+                        </a>
+                    </div>
 
                 </div>
 
