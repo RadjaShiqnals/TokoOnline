b76fe97acde2c48c95f175a1a743e86220b300fe
RadjaShiqnals <radjagenta@gmail.com>
2026-07-16 12:36:25 +0700

    style: update register screen loading layout

--- register.php
+++ register.php
@@ -93,8 +93,10 @@ session_start(); // creating new session
             left: 0;
             width: 100%;
             height: 100%;
-            background-color: #ffffff;
-            /* Change this to your desired background color */
+            background-color: rgba(4, 0, 68, 0.7);
+            backdrop-filter: blur(8px);
+            -webkit-backdrop-filter: blur(8px);
+            color: #ffffff;
             display: flex;
             justify-content: center;
             align-items: center;
@@ -103,7 +105,7 @@ session_start(); // creating new session
 
         /* Loading Animation time */
         #loading-screen {
-            transition: opacity 0.25s ease;
+            transition: opacity 0.15s ease;
             /* Add a transition for opacity */
             opacity: 1;
             /* Start with full opacity */
@@ -115,7 +117,7 @@ session_start(); // creating new session
 <body>
     <!--\| Loading Screen -->
     <div id="loading-screen">
-        <div class="spinner-border" role="status">
+        <div class="spinner-border text-light" role="status">
         </div>
         <p style="margin-left:10px;font-weight: bold;font-size: 1.8em;">Loading...</p>
     </div>
